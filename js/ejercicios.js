08138fca19688c0d079643eb84fde473dbcff149
Sobrevivinsky <matias.zubrzycki@gmail.com>
2024-11-19 23:44:21 -0300

    primer commit

---
+++ js/ejercicios.js
@@ -0,0 +1,61 @@
+
+//ejercicios 1 de Profe alejandro//
+
+
+let nombre = "Matias";
+let edad = 43;
+
+
+console.warn ("Todavia no se puede decir que entiendo mucho sobre JS")
+
+
+console.log(nombre);
+console.log(edad);
+
+
+let numero1 = 5;
+let numero2 = 3;
+let suma = numero1 + numero2;
+let resultadoSuma = suma;
+
+let numero3 = 10;
+let numero4 = 7;
+let resta = numero3 - numero4;
+let resultadoResta = resta;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+
+//ejercicio8//
+let numero5 = 8;
+let numero6 = 5;
+let multiplicacion = numero5 * numero6;
+let resultadoMulti = multiplicacion;
+
+console.log(resultadoMulti);
+
+// ejercicio 9 //
+let numero7 = 20;
+let numero8 = 4;
+let division = numero7 / numero8;
+let resultadoDivision = division;
+
+console.log(resultadoDivision);
+
+
+//operaciones simplificadas (al principio me la complique alpedo)//
+
+let resultadoSuma1 = 5 + 3;
+console.log(resultadoSuma1)
+
+//ejercicio 11 //
+
+let texto1 = "como estas "
+let texto2 = "Chupachichi?"
+
+console.log(texto1 + texto2)
+
+// PROMPT EJERCICIOS //
+
+
+
