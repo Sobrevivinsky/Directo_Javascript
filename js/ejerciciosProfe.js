d740267b4f1869281f6aeb1d6f541b2f9e3d5b2e
Sobrevivinsky <matias.zubrzycki@gmail.com>
2024-11-26 16:39:43 -0300

    nuevos ejercicios

---
+++ js/ejerciciosProfe.js
@@ -0,0 +1,193 @@
+
+//ejercicios 1 de Profe alejandro//
+
+/*
+
+let nombre = "Matias";
+let edad = 43;
+
+
+console.warn ("Todavia no se puede decir que entiendo mucho sobre JS")
+
+
+console.log(nombre);
+console.log(edad);
+
+
+let numero1 = 5;
+let numero2 = 3;
+let suma = numero1 + numero2;
+let resultadoSuma = suma;
+
+let numero3 = 10;
+let numero4 = 7;
+let resta = numero3 - numero4;
+let resultadoResta = resta;
+
+console.log(resultadoSuma);
+console.log(resultadoResta);
+
+//ejercicio8//
+let numero5 = 8;
+let numero6 = 5;
+let multiplicacion = numero5 * numero6;
+let resultadoMulti = multiplicacion;
+
+console.log(resultadoMulti);
+
+// ejercicio 9 //
+let numero7 = 20;
+let numero8 = 4;
+let division = numero7 / numero8;
+let resultadoDivision = division;
+
+console.log(resultadoDivision);
+
+
+//operaciones simplificadas (al principio me la complique alpedo)//
+
+let resultadoSuma1 = 5 + 3;
+console.log(resultadoSuma1)
+
+//ejercicio 11 //
+
+let texto1 = "como estas "
+let texto2 = "Chupachichi?"
+
+console.log(texto1 + texto2)
+
+// PROMPT EJERCICIOS + alert + confirm //
+
+// 12 //
+
+let nombreDeUsuario = prompt("Ingrese su nombre: ");
+//console.log(nombreDeUsuario);
+console.log("El nombre ingresado es: " + nombreDeUsuario);
+
+//13//
+let edadDeUsuario = prompt("Ingrese su edad: ");
+
+console.log("La edad ingresada es: " + edadDeUsuario);
+
+//14//
+let numeroA = parseInt(prompt("Ingrese el primer numero: "));
+let numeroB = parseInt(prompt("Ingrese el segundo numero: "));
+let resultado1 = numeroA + numeroB;
+
+console.log("El resultado es: " + resultado1);
+
+//15//
+let colorFavorito = prompt("Ingrese su color favorito: ");
+console.log("Su color favorito es: " + colorFavorito);
+
+//16//
+let fraseUsuario = prompt("Ingrese su frase ");
+
+console.log("Su frase es: " + fraseUsuario); 
+
+//17//
+let ciudadDeNacimiento = prompt("Ciudad dónde naciste: ");
+
+console.log("Su ciudad natal es: " + ciudadDeNacimiento);
+
+//18//
+
+//Alert//
+
+
+//22//
+alert("Bienvenido a la página web! ");
+
+//23//
+let comidaFavorita = prompt("¿Cuál es tu comida favorita?: ");
+
+alert("Su comida favorita es: " + comidaFavorita);
+
+//24 - 26//
+alert("¡Buenos días!");
+alert("¡Buenas tardes!");
+alert("¡Buenas noches!");
+
+//27//
+
+let animalFavorito = prompt("¿Cúal es tu animal favorito? ");
+alert("Su animal favorito es: " + animalFavorito);
+
+//28//
+alert("Feliz cumpleaños!!");
+
+//29//
+let bebidaFavorita = prompt("¿Cúal es su bebida alcohólica favorita?");
+
+alert("Su escabio favorito es: " + bebidaFavorita);
+
+//30//
+
+alert("¡Feliz año Nuevo del sur!!!");
+
+//31//
+
+let estacionFavorita = prompt("¿Cúal es tu estación del año preferida?");
+alert("Su estación del año preferida es: " + estacionFavorita);
+
+*/
+/*
+
+// 4. COMBINACION CONSOLA PROMPT ALERT //
+
+//32//
+
+let nombreUsuario1 = prompt("Ingrese su nombre");
+let edadUsuario1 = prompt("Ingrese su edad");
+
+//33//
+
+alert("Su nombre es: " + nombreUsuario1 + " y su edad es: " + edadUsuario1);
+
+//34//
+
+const añoEnCurso = 2024;
+
+let calculoNacimiento = añoEnCurso - edadUsuario1;
+console.log("Su año de nacimiento es: " + calculoNacimiento);
+
+//35//
+
+if(edadUsuario1 >= 18) {
+    alert("Ud. es mayor de edad, puede ingresar");
+} else if(edadUsuario1 <= 17) {
+    alert("Ud. es menor de edad, no puede ingresar");
+}
+
+//36//
+let numeroUsuarioA = parseInt(prompt("Ingrese el primer numero: "));
+let numeroUsuarioB = parseInt(prompt("Ingrese el segundo numero: "));
+
+let resultadoSuma = numeroUsuarioA + numeroUsuarioB;
+
+alert("La suma de los dos números da: " + resultadoSuma);
+
+//37//
+
+let nombreUsuario2 = prompt("Ingrese su nombre: ");
+console.log("Hola " + nombreUsuario2);
+
+*/
+
+//39//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/miarchivo.js
@@ -0,0 +1,17 @@
+// EJERCICIO de FUNDAMENTOS DE JAVASCRIPT //
+
+/* let numeroUsuario = prompt("Ingrese un numero del 1 al 20:");
+
+for(let i = numeroUsuario; i + 1; i++) {
+    if(i >= 20) {
+        break;
+    }
+    alert("resultado " + i); 
+} */
+
+let i = 0;
+while (i < 11) {
+    alert("numero: " + i);
+    i++;
+}
+
